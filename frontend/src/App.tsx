9189c7c6c5a8292ed9748b859b82af65fb18c71f
Helcio Itiyama <helcio.itiyama@gmail.com>
2020-08-02 18:31:37 -0300

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyles from './styles/global';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <SignUp />
+      <GlobalStyles />
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça o cadastro</h1>
+
+          <Input name="email" type="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
